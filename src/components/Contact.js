b622fbc7df8cea5b4add70a203a717e5d3968564
Laya Samizadeh <laya.samizadeh@partsoftware.com>
2023-08-20 08:19:26 +0330

    improment in contact props passing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
   //   phone: PropTypes.string.isRequired,
   // };
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>{name}</h4>
@@ -21,9 +21,10 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
+  // name: PropTypes.string.isRequired,
+  // email: PropTypes.string.isRequired,
+  // phone: PropTypes.string.isRequired,
+  contact : PropTypes.object.isRequired
 };
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -31,9 +31,7 @@ class Contacts extends Component {
             <div>
                 {contacts.map(contact =>
                     <Contact key={contact.id}
-                        name ={contact.name}
-                        email = {contact.email}
-                        phone = {contact.phone}
+                        contact ={contact}
                     /> )}
             </div>
         )
