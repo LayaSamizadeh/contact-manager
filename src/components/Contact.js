1019bc1101da9a165e43ea15ade49ababbc29520
Laya Samizadeh <laya.samizadeh@partsoftware.com>
2023-09-10 08:39:52 +0330

    evnts in react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 class Contact extends Component {
+
+  // constructor() {
+  //   super();
+  //   this.state = {}
+
+  //   this.onShowClick = this.onShowClick.bind(this);  
+  //   /*for using this inside a functional component of class */
+  // }
+
   // static propTypes = {
   //   name: PropTypes.string.isRequired,
   //   email: PropTypes.string.isRequired,
   //   phone: PropTypes.string.isRequired,
   // };
+
+  state = {}
+
+  onShowClick = e => {        // arrow function do the same binding this 
+    console.log(e.target);
+  }
+
   render() {
     const { name, email, phone } = this.props.contact;
+
+    // for using this inside a functional component of class : onClick={this.onShowClick.bind(this)}
+
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>{name}
+          <i onClick={this.onShowClick} className=" fas fa-sort-down"></i>
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
         </ul>
-      </div>
+      </div >
     );
   }
 }
@@ -24,7 +45,7 @@ Contact.propTypes = {
   // name: PropTypes.string.isRequired,
   // email: PropTypes.string.isRequired,
   // phone: PropTypes.string.isRequired,
-  contact : PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
