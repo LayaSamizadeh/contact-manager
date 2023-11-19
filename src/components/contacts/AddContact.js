09cea9b848595b50621689347eee35ad6b61af5d
Laya Samizadeh <laya.samizadeh@partsoftware.com>
2023-11-19 14:50:22 +0330

    add on submit for form

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -8,6 +8,11 @@ class AddContact extends Component {
         phone: ''
     };
 
+    onSumbit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     onChange = e => this.setState({ [e.target.name]: e.target.value})
 
     render() {
@@ -16,7 +21,7 @@ class AddContact extends Component {
             <div className="card mb-3">
                 <div className="card-header">Add Contact</div>
                 <div className="card-body">
-                    <form action="">
+                    <form onSubmit={this.onSumbit}>
                         <div className="form-group">
                             <label htmlFor="name">
                                 Name
@@ -56,7 +61,7 @@ class AddContact extends Component {
                                 onChange={this.onChange}
                             />
                         </div>
-                        <input type="submit" value="Add Contact" className="btn btn-light btn-block" />
+                        <input type="submit" value="Add Contact" className="btn btn-light btn-block"  />
                     </form>
                 </div>
             </div>
