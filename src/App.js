46104d7e8bc1f0ecc463a58efbe1a2a6b1e3b118
Laya Samizadeh <laya.samizadeh@partsoftware.com>
2024-03-26 11:10:07 +0330

    add routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
-// import Contact from "./components/Contact";
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
 import AddContact from "./components/contacts/AddContact";
+import About from "./components/pages/About";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./context";
 function App() {
   return (
     <Provider>
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <AddContact />
-          <Contacts />
+      <Router>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" Component={Contacts} />
+              <Route exact path="/contact/add" Component={AddContact} />
+              <Route exact path="/about" Component={About} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     </Provider>
   );
 }
